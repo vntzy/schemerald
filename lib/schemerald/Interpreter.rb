3bc5314109f2504ef591c4d7f13ad67278020e07
Ventsislav <vntzyv@gmail.com>
2014-02-05 00:09:14 +0200

    add some defaults

--- lib/schemerald/Interpreter.rb
+++ lib/schemerald/Interpreter.rb
@@ -1,5 +1,16 @@
 class Interpreter
+  DEFAULTS = {
+    :+ => lambda {|*args| args.reduce(:+) },
+    :- => lambda {|*args| args.reduce(:-) },
+    :* => lambda {|*args| args.reduce(:*) },
+    :/ => lambda {|*args| args.reduce(:/) },
+    :car => lambda {|x| x.car },
+    :cdr => lambda {|x| x.cdr },
+    :cons => lambda {|x, y| Cons.new(x, y) },
+    :list => lambda {|*args| args.consify },
+  }
   def initialize
+    @environment = Environment.new(nil, DEFAULTS)
   end
 
   def read
