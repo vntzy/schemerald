ae146d14b0c03f9da38583c7fff7f976cbac8559
Ventsislav <vntzyv@gmail.com>
2014-02-05 02:21:12 +0200

    fix cons default

--- lib/schemerald/Interpreter.rb
+++ lib/schemerald/Interpreter.rb
@@ -6,7 +6,7 @@ class Interpreter
     :/ => lambda {|*args| args.reduce(:/) },
     :car => lambda {|x| x.car },
     :cdr => lambda {|x| x.cdr },
-    :cons => lambda {|x, y| Cons.new(x, y) },
+    :cons => lambda {|x, y| Cons.new(x, Cons.new(y, :nil)) },
     :list => lambda {|*args| args.consify },
   }
   FORMS = {}
